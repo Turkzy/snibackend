574bc007801115ef93562279d5420984b07edb49
Turkzy <165537971+Turkzy@users.noreply.github.com>
2024-06-30 20:48:28 +0800

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('inventory','root','', {
+const db = new Sequelize('id22389075_inventory','id22389075_sison0422','Sison_0422', {
     host: 'localhost',
     dialect: "mysql"
 });
